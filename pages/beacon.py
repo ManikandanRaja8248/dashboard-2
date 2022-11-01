d5a1b6acbad0fc3e4c3edff3efb6e93d2e4b36d3
deebalakshm <deebalakshmi.m@vicara.co>
2022-11-01 21:02:20 +0530

    backend integrated with frontend

--- pages/beacon.py
+++ pages/beacon.py
@@ -1,18 +1,25 @@
+from email.mime import base
 import json
+from urllib import response
 from dash import html, dcc, callback_context
 from dash.dependencies import Input, Output, State, ALL, MATCH
 from dash.exceptions import PreventUpdate
 import requests
 import dash_bootstrap_components as dbc
 from app import app
+import requests
+import json
 
-data={
-    "beaconID":"001",
-    "licensePlateNumber":"2222",
-    "zone":"zone A",
-    "movingStatus":"ideal",
-    "batteryLevel":"40%",   
-}
+baseUrl = "http://52.74.198.240:60009"
+entity = "/beacon"
+
+def getBeacons():
+    url = baseUrl+entity
+    response = requests.get(url)
+    data = response.content.decode()
+    return data
+
+data = json.loads(getBeacons())
 
 layout = html.Div(className="current-status-container",children=[
     html.Div(className="heading",children=[""]),
@@ -21,23 +28,23 @@ layout = html.Div(className="current-status-container",children=[
          html.Div(className="table-column",children=["Beacon ID"]),
          html.Div(className="table-column",children=["License Plate Number"]),
          html.Div(className="table-column",children=["Zone"]),
-         html.Div(className="table-column",children=["Moving Status"]),
+        #  html.Div(className="table-column",children=["Moving Status"]),
          html.Div(className="table-column",children=["Battery Level"]),
     ]) ,
 
-    html.Div(children=[
+    html.Div(className="beacon-container",children=[
          html.Div(children=[
          html.Div(className ="becon-table-row",children=[
-         html.Div(className="table-column",children=[data["beaconID"]]),
-         html.Div(className="table-column",children=[data["licensePlateNumber"]]),
-         html.Div(className="table-column",children=[data["zone"]]),
-         html.Div(className="table-column",children=[data["movingStatus"]]),
+         html.Div(className="table-column",children=[data[0]["beacon_id"]]),
+         html.Div(className="table-column",children=[data[0]["license_plate_number"]]),
+         html.Div(className="table-column",children=[data[0]["zone"]]),
+        #  html.Div(className="table-column",children=["movingStatus"]),
          html.Div(className="table-column battery-div",children=[
              html.I(className="bi bi-battery-full"),
-             html.Div(children=[data["batteryLevel"]]),
+             html.Div(children=[data[0]["battery_level"]]),
              ]),
     ]) 
-    ],className="becon-table")for i in range (5)  
+    ],className="becon-table")for i in range (10)  
     ])
    
     
--- pages/events.py
+++ pages/events.py
@@ -1,32 +1,42 @@
 from dash import html,dcc,Input,Output
 from app import app
+import requests
+import json
 
 
+baseUrl = "http://52.74.198.240:60009"
 
 
-
-data = {
-    "model1":{"event":"one","timestamp":"29/10/2022","zone":"Zone A"},
-    "model2":{"event":"two","timestamp":"30/10/2022","zone":"Zone B"},
-    "model3":{"event":"three","timestamp":"31/10/2022","zone":"Zone C"},
-}
-modelList = list(data.keys())
-print(modelList[0])
-
+def getAllCars():
+    entity = "/dashboard/get_all_cars"
+    url = baseUrl+entity
+    response = requests.get(url)
+    data = response.content.decode()
+    return data
+carList = json.loads(getAllCars())
 
 @app.callback(Output("event-data","children"),
               Input("event-dropdown","value"),
               )
 def displayEvents(value):
-    if value in data:
-        return html.Div(className = "event-status",children=[html.Div(className="data-field",children=[data[value]["timestamp"]]),
-                                            html.Div(className="data-field",children=[data[value]["zone"]]),
-                                            html.Div(className="data-field",children=[data[value]["event"]]),]),
+    if value in carList:
+        entity = f"/events/{value}"
+        url = baseUrl+entity
+        response = requests.get(url)
+        data = response.content.decode()
+        events = json.loads(data)
+    
+        return html.Div(className="event-data-field-container",children=[
+            html.Div(className = "event-status",children=[html.Div(className="data-field",children=[events[i]['timestamp']]),
+                                            html.Div(className="data-field",children=[events[i]['gateway_id']]),
+                                            html.Div(className="data-field",children=[events[i]['event']])
+                                            ])for i in range(len(events))
 
+        ])
 layout =html.Div(className="events-container",children=[
     dcc.Dropdown(id = "event-dropdown",
-        options=modelList,
-        value=modelList[0],className="model-dropdown"
+        options=carList,
+        value=carList[0],className="model-dropdown"
     ),
     html.Div(className="event-container",children=[
         html.Div(className= "event-heading",children=[html.Div(className="event-field",children=["Timestamp"]),
